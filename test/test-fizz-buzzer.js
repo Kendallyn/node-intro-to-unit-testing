13da715aead16bab5218ba7dd3b29e3cd8baf5f0
Kendallyn <Kendallynlyon@hotmail.com>
2017-05-08 10:19:44 -0700

    corrected comma

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -3,17 +3,17 @@ const should = require('chai').should();
 const fizzBuzzer = require('../fizzBuzzer');
 
 describe('fizzBuzzer', function () {
-    it('should return "fizz-buzz" for multiples of 15'function () {
+    it('should return "fizz-buzz" for multiples of 15', function () {
         [15, 30, 45].forEach(function (input) {
             fizzBuzzer(input).should.equal('fizz-buzz');
         });
     });
-    it('should return "fizz" for multiples of 3'function () {
+    it('should return "fizz" for multiples of 3', function () {
         [3, 6, 9, 12].forEach(function (input) {
             fizzBuzzer(input).should.equal('fizz');
         });
     });
-    it('should return "buzz" for multiples of 5'function () {
+    it('should return "buzz" for multiples of 5', function () {
                 [5, 10, 15].forEach(function (input) {
             fizzBuzzer(input).should.equal('buzz');
         });
